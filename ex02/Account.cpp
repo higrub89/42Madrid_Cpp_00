27b80b722a530e7458751778f796e1237f9d3f8b
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2026-01-04 00:28:31 +0100

    Creacion de funciones _displayTimestamp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,3 +6,38 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
+// Getters estáticos - retornan los valores de las variables estáticas
+int Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
+Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
+    _accountIndex = _nbAccounts;
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+void Account::_displayTimestamp(void) {
+    std::time_t t = std::time(NULL);
+    char buf[20];
+    std::strftime(buf, sizeof(buf), "[%Y%m%d_%H%M%S] ", std::localtime(&t));
+    std::cout << buf;
+}
